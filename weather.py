c16f0866c5080db03c86822855289b4353727fd4
William Lee <williamlee0416@gmail.com>
2024-10-20 20:55:55 -0700

    Simple weather API get request for Berkeley forecast

--- weather.py
+++ weather.py
@@ -1,6 +1,27 @@
 from dotenv import load_dotenv
 import os
+import requests, urllib3
 
-load_dotenv() # API Key
+load_dotenv()
 api_key = os.getenv("API_KEY")
-print(api_key)
+
+base_url = "http://api.weatherapi.com/v1"
+forecast_url = base_url + "/forecast.json"
+
+params = {
+    "key": api_key,
+    "q": 94704, # Berkeley ZIP code
+    "days": 5,
+}
+
+response = requests.get(forecast_url, params=params)
+
+if response.status_code == 200:
+    response = response.json()
+    location = response["location"]
+else:
+    print("Unsuccesful forecast request")
+
+print(f"Located in {location["name"]} at: ({location["lat"]}, {location["lon"]})")
+
+print(response["forecast"])
